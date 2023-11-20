5dc31fa2ba003440e813184db193cc4a7be0e1b2
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-11-20 13:26:11 -0300

    footer

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,11 +1,13 @@
+import { Footer } from './_components/footer'
 import { Navbar } from './_components/navbar'
 
 const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full bg-slate-100">
+      <Navbar />
       <main className="pt-40 pb-20 bg-slate-100"></main>
       {children}
-      <Navbar />
+      <Footer />
     </div>
   )
 }

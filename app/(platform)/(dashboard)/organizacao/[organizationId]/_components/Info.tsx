98d01167c108b5cf9698f68427178dfa0d14c22b
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-11-22 17:32:39 -0300

    info component and refactoring org id page

---
+++ app/(platform)/(dashboard)/organizacao/[organizationId]/_components/Info.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import Image from 'next/image'
+import { useOrganization } from '@clerk/nextjs'
+import { CreditCard } from 'lucide-react'
+import { Skeleton } from '@/components/ui/skeleton'
+
+export const Info = () => {
+  const { organization, isLoaded } = useOrganization()
+
+  if (!isLoaded) return <Info.Skeleton />
+
+  return (
+    <div className="flex items-center gap-4">
+      <div className="w-[60px] h-[60px] relative">
+        <Image
+          fill
+          src={organization?.imageUrl!}
+          alt="Organização"
+          className="rounded-md object-cover"
+        />
+      </div>
+      <div>
+        <p className="font-semibold text-xl">{organization?.name}</p>
+        <div className="flex items-center text-xs text-muted-foreground">
+          <CreditCard className="h-3 w-3 mr-1" />
+          Grátis
+        </div>
+      </div>
+    </div>
+  )
+}
+
+Info.Skeleton = function SkeletonInfo() {
+  return (
+    <div className="flex items-center gap-4">
+      <div className="w-[60px] h-[60px] relative">
+        <Skeleton className="w-full h-full absolute" />
+      </div>
+      <div className="space-y-2">
+        <Skeleton className="h-10 w-[200px]" />
+        <div className="flex items-center">
+          <Skeleton className="h-4 w-4 mr-2" />
+          <Skeleton className="h-4 w-[100px]" />
+        </div>
+      </div>
+    </div>
+  )
+}

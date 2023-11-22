bb63738e8f1c1fe7e669422502362972708917eb
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-11-22 15:56:46 -0300

    new action for create board

---
+++ actions/create-board/index.ts
@@ -0,0 +1,42 @@
+'use server'
+import { revalidatePath } from 'next/cache'
+
+import { auth } from '@clerk/nextjs'
+import { db } from '@/lib/db'
+
+import { InputType, ReturnType } from './types'
+import { CreateSafeAction } from '@/lib/create-safe-action'
+import { CreateBoard } from './schema'
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId } = auth()
+
+  if (!userId) {
+    return {
+      error: 'Usuário não autorizado',
+    }
+  }
+
+  const { title } = data
+
+  let board
+
+  try {
+    board = await db.board.create({
+      data: {
+        title,
+      },
+    })
+  } catch (error) {
+    return {
+      error: 'Falha ao criar',
+    }
+  }
+
+  revalidatePath(`/board/${board.id}`)
+  return {
+    data: board,
+  }
+}
+
+export const createBoard = CreateSafeAction(CreateBoard, handler)
---
+++ actions/create-board/schema.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod'
+
+export const CreateBoard = z.object({
+  title: z
+    .string({
+      required_error: '´Titulo é obrigatório',
+      invalid_type_error: 'Titulo é obrigatório',
+    })
+    .min(3, {
+      message: 'Titulo é muito pequeno',
+    }),
+})

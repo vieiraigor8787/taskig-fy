7b16e7b73ef8b1b30a0d5ef9de07e45d42f4accf
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-11-24 13:26:54 -0300

    board page | meta title

--- app/(platform)/(dashboard)/organizacao/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organizacao/[organizationId]/layout.tsx
@@ -1,4 +1,16 @@
+import { startCase } from 'lodash'
+
+import { auth } from '@clerk/nextjs'
+
 import { OrgControl } from './_components/OrgControl'
+
+export async function generateMetadata() {
+  const { orgSlug } = auth()
+
+  return {
+    title: startCase(orgSlug || 'organizacao'),
+  }
+}
 
 const OrganizationIdLayout = ({ children }: { children: React.ReactNode }) => {
   return (

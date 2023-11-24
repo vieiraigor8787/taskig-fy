f979603493faf075131a7f7f6124549237a4cfb7
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-11-24 18:46:09 -0300

    list component | button enabling input to edit

--- app/(platform)/(dashboard)/board/_components/ListForm.tsx
+++ app/(platform)/(dashboard)/board/_components/ListForm.tsx
@@ -1,14 +1,19 @@
 'use client'
 
 import { ElementRef, useRef, useState } from 'react'
-import { PlusIcon } from 'lucide-react'
+import { useParams } from 'next/navigation'
 import { useEventListener, useOnClickOutside } from 'usehooks-ts'
+import { Plus, X } from 'lucide-react'
 
 import { FormInput } from '@/components/form/FormInput'
+import { FormSubmit } from '@/components/form/FormButton'
+import { Button } from '@/components/ui/button'
 
 import { ListWrapper } from './ListWrapper'
 
 export const ListForm = () => {
+  const params = useParams()
+
   const [isEditing, setIsEditing] = useState(false)
 
   const formRef = useRef<ElementRef<'form'>>(null)
@@ -43,7 +48,15 @@ export const ListForm = () => {
             ref={inputRef}
             id="title"
             className="text-sm px-2 py-1 h-7 font-medium border-transparent hover:border-input focus:border-input transperent"
+            placeholder="Insira um nome"
           />
+          <input hidden value={params.boardId} name="boardId" />
+          <div className="flex items-center justify-between gap-1">
+            <FormSubmit>Adicionar lista</FormSubmit>
+            <Button onClick={disableEditing} size="sm" variant="ghost">
+              <X className="w-5 h-5" />
+            </Button>
+          </div>
         </form>
       </ListWrapper>
     )
@@ -56,7 +69,7 @@ export const ListForm = () => {
           onClick={enableEditing}
           className="w-full rounded-md bg-white/80 hover:bg-white/50 transition p-3 flex items-center font-medium text-sm"
         >
-          <PlusIcon className="w-4 h-4 mr-2" />
+          <Plus className="w-4 h-4 mr-2" />
           Adicionar uma lista
         </button>
       </form>

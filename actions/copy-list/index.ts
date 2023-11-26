a8b4f547727c0e1ba698585abad11de7b946c437
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-11-26 12:19:04 -0300

    list options | actions - copy list

---
+++ actions/copy-list/index.ts
@@ -0,0 +1,79 @@
+'use server'
+import { revalidatePath } from 'next/cache'
+
+import { auth } from '@clerk/nextjs'
+import { db } from '@/lib/db'
+import { CreateSafeAction } from '@/lib/create-safe-action'
+
+import { InputType, ReturnType } from './types'
+import { CopyList } from './schema'
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId, orgId } = auth()
+
+  if (!userId || !orgId) {
+    return {
+      error: 'Usuário não autorizado',
+    }
+  }
+
+  const { id, boardId } = data
+
+  let list
+
+  try {
+    const listToCopy = await db.list.findUnique({
+      where: {
+        id,
+        boardId,
+        board: {
+          orgId,
+        },
+      },
+      include: {
+        cards: true,
+      },
+    })
+
+    if (!listToCopy) {
+      return { error: 'Lista não encontrada' }
+    }
+
+    const lastList = await db.list.findFirst({
+      where: { boardId },
+      orderBy: { order: 'desc' },
+      select: { order: true },
+    })
+
+    const newOrder = lastList ? lastList.order + 1 : 1
+
+    list = await db.list.create({
+      data: {
+        boardId: listToCopy.boardId,
+        title: `${listToCopy.title} - cópia`,
+        order: newOrder,
+        cards: {
+          createMany: {
+            data: listToCopy.cards.map((card) => ({
+              title: card.title,
+              description: card.description,
+              order: card.order,
+            })),
+          },
+        },
+      },
+      include: {
+        cards: true,
+      },
+    })
+  } catch (error) {
+    return {
+      error: 'Falha ao copiar.',
+    }
+  }
+
+  revalidatePath(`/board/${boardId}`)
+
+  return { data: list }
+}
+export const copyList = CreateSafeAction(CopyList, handler)
--- app/(platform)/(dashboard)/board/_components/ListOptions.tsx
+++ app/(platform)/(dashboard)/board/_components/ListOptions.tsx
@@ -1,10 +1,13 @@
 'use client'
 
 import { MoreHorizontal, X } from 'lucide-react'
+import { ElementRef, useRef } from 'react'
+import { toast } from 'sonner'
 
 import { List } from '@prisma/client'
 import { useAction } from '@/hooks/use-action'
 import { deleteList } from '@/actions/delete-list'
+import { copyList } from '@/actions/copy-list'
 
 import { Button } from '@/components/ui/button'
 import {
@@ -15,7 +18,6 @@ import {
 } from '@/components/ui/popover'
 import { FormSubmit } from '@/components/form/FormButton'
 import { Separator } from '@/components/ui/separator'
-import { toast } from 'sonner'
 
 interface ListOptionsProps {
   data: List
@@ -23,9 +25,20 @@ interface ListOptionsProps {
 }
 
 export const ListOptions = ({ onAddCard, data }: ListOptionsProps) => {
+  const closeRef = useRef<ElementRef<'button'>>(null)
+
   const { execute: executeDelete } = useAction(deleteList, {
     onSuccess: (data) => {
       toast.success(`Lista "${data.title}" excluída.`)
+      closeRef.current?.click()
+    },
+    onError: (err) => toast.error(err),
+  })
+
+  const { execute: executeCopy } = useAction(copyList, {
+    onSuccess: (data) => {
+      toast.success(`Lista "${data.title}" copiada.`)
+      closeRef.current?.click()
     },
     onError: (err) => toast.error(err),
   })
@@ -35,6 +48,13 @@ export const ListOptions = ({ onAddCard, data }: ListOptionsProps) => {
     const boardId = formData.get('boardId') as string
 
     executeDelete({ id, boardId })
+  }
+
+  const onCopy = (formData: FormData) => {
+    const id = formData.get('id') as string
+    const boardId = formData.get('boardId') as string
+
+    executeCopy({ id, boardId })
   }
 
   return (
@@ -48,7 +68,7 @@ export const ListOptions = ({ onAddCard, data }: ListOptionsProps) => {
         <div className="text-sm font-medium text-center text-neutral-600 pb-4">
           Ações da lista
         </div>
-        <PopoverClose asChild>
+        <PopoverClose asChild ref={closeRef}>
           <Button
             className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600"
             variant="ghost"
@@ -64,7 +84,7 @@ export const ListOptions = ({ onAddCard, data }: ListOptionsProps) => {
           Adicionar cartão...
         </Button>
         <Separator />
-        <form>
+        <form action={onCopy}>
           <input hidden name="id" id="id" value={data.id} />
           <input hidden name="boardId" id="boardId" value={data.boardId} />
           <FormSubmit

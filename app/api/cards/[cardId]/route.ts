1a27eb9857aa0a0c5819c1039edfd155d2c1def5
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-11-27 09:34:26 -0300

    card modal | add route for card id

---
+++ app/api/cards/[cardId]/route.ts
@@ -0,0 +1,34 @@
+import { db } from '@/lib/db'
+import { auth } from '@clerk/nextjs'
+import { NextResponse } from 'next/server'
+
+export async function GET(
+  req: Request,
+  { params }: { params: { cardId: string } }
+) {
+  const { userId, orgId } = auth()
+
+  if (!userId || !orgId) {
+    return new NextResponse('Não autorizado', { status: 401 })
+  }
+
+  const card = await db.card.findUnique({
+    where: {
+      id: params.cardId,
+      list: {
+        board: {
+          orgId,
+        },
+      },
+    },
+    include: {
+      list: {
+        select: {
+          title: true,
+        },
+      },
+    },
+  })
+
+  return NextResponse.json(card)
+}

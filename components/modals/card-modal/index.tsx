17a7da1f011ffcc90b37c699fb3908a50a8fd501
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-11-27 09:38:13 -0300

    card modal | showing card name into modal

--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -1,16 +1,25 @@
 'use client'
 
-import { Dialog, DialogContent } from '@/components/ui/dialog'
+import { useQuery } from '@tanstack/react-query'
+
+import { CardWithList } from '@/types'
+import { fetcher } from '@/lib/fetcher'
 import { useCardModal } from '@/hooks/use-card-modal'
+import { Dialog, DialogContent } from '@/components/ui/dialog'
 
 export const CardModal = () => {
   const id = useCardModal((state) => state.id)
   const isOpen = useCardModal((state) => state.isOpen)
   const onClose = useCardModal((state) => state.onClose)
 
+  const { data: cardData } = useQuery<CardWithList>({
+    queryKey: ['card', id],
+    queryFn: () => fetcher(`/api/cards/${id}`),
+  })
+
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-      <DialogContent></DialogContent>
+      <DialogContent>{cardData?.title}</DialogContent>
     </Dialog>
   )
 }

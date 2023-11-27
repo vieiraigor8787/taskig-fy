1cc09f737fc59189ea84d2166b39e4ca30627c2c
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-11-26 22:59:12 -0300

    drag n drop | moving an list

--- app/(platform)/(dashboard)/board/_components/ListContainer.tsx
+++ app/(platform)/(dashboard)/board/_components/ListContainer.tsx
@@ -12,6 +12,14 @@ interface ListContainerProps {
   boardId: string
 }
 
+function reorder<T>(list: T[], startIndex: number, endIndex: number) {
+  const result = Array.from(list)
+  const [removed] = result.splice(startIndex, 1)
+  result.splice(endIndex, 0, removed)
+
+  return result
+}
+
 export const ListContainer = ({ data, boardId }: ListContainerProps) => {
   const [orderedData, setOrderedData] = useState(data)
 
@@ -19,8 +27,28 @@ export const ListContainer = ({ data, boardId }: ListContainerProps) => {
     setOrderedData(data)
   }, [data])
 
+  const onDragEnd = (result: any) => {
+    const { destination, source, type } = result
+
+    if (!destination) return
+
+    //se arrastado na mesma posição
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    )
+      return
+
+    //movendo uma lista
+    if (type === 'list') {
+      const items = reorder(orderedData, source.index, destination.index).map(
+        (item, index) => ({ ...item, order: index })
+      )
+      setOrderedData(items)
+    }
+  }
   return (
-    <DragDropContext onDragEnd={() => {}}>
+    <DragDropContext onDragEnd={onDragEnd}>
       <Droppable droppableId="lists" type="list" direction="horizontal">
         {(provided) => (
           <ol

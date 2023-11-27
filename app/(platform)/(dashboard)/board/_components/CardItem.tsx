ac074dbb7f2c73a524ca69ebb17e8c596b708693
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-11-26 22:52:09 -0300

    drag n drop | pangea  add in needed components

--- app/(platform)/(dashboard)/board/_components/CardItem.tsx
+++ app/(platform)/(dashboard)/board/_components/CardItem.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { Card } from '@prisma/client'
+import { Draggable, Droppable } from '@hello-pangea/dnd'
 
 interface CardItemProps {
   data: Card
@@ -9,11 +10,18 @@ interface CardItemProps {
 
 export const CardItem = ({ index, data }: CardItemProps) => {
   return (
-    <div
-      role="button"
-      className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white"
-    >
-      {data.title}
-    </div>
+    <Draggable draggableId={data.id} index={index}>
+      {(provided) => (
+        <div
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+          role="button"
+          className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white"
+        >
+          {data.title}
+        </div>
+      )}
+    </Draggable>
   )
 }
--- app/(platform)/(dashboard)/board/_components/ListContainer.tsx
+++ app/(platform)/(dashboard)/board/_components/ListContainer.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from 'react'
 import { ListWithCards } from '@/types'
+import { DragDropContext, Droppable } from '@hello-pangea/dnd'
 
 import { ListForm } from './ListForm'
 import { ListItem } from './ListItem'
@@ -19,12 +20,23 @@ export const ListContainer = ({ data, boardId }: ListContainerProps) => {
   }, [data])
 
   return (
-    <ol className="flex gap-3 h-full">
-      {orderedData.map((list, index) => {
-        return <ListItem key={list.id} data={list} index={index} />
-      })}
-      <ListForm />
-      <div className="flex-shrink-0 w-1" />
-    </ol>
+    <DragDropContext onDragEnd={() => {}}>
+      <Droppable droppableId="lists" type="list" direction="horizontal">
+        {(provided) => (
+          <ol
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+            className="flex gap-3 h-full"
+          >
+            {orderedData.map((list, index) => {
+              return <ListItem key={list.id} data={list} index={index} />
+            })}
+            {provided.placeholder}
+            <ListForm />
+            <div className="flex-shrink-0 w-1" />
+          </ol>
+        )}
+      </Droppable>
+    </DragDropContext>
   )
 }
